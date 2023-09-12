1f1be7e004186ca41e6cd7045f14719a3d6c7f6d
AurelFi <aurelien.fiche.esipe@gmail.com>
2023-09-12 11:05:36 +0200

    Mapper-Q2

--- mapper/src/main/java/com/github/forax/framework/mapper/JSONWriter.java
+++ mapper/src/main/java/com/github/forax/framework/mapper/JSONWriter.java
@@ -1,11 +1,27 @@
 package com.github.forax.framework.mapper;
 
+import java.beans.BeanInfo;
+import java.beans.PropertyDescriptor;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public final class JSONWriter {
   public String toJSON(Object o) {
     return switch (o) {
       case null -> "null";
       case String s -> '"' + s + '"';
-      default -> String.valueOf(o);
+      case Boolean b -> b.toString();
+      case Integer i -> i.toString();
+      case Double d -> d.toString();
+      default -> beanToJson(o);
     };
+  }
+
+  private String beanToJson(Object o) {
+    PropertyDescriptor[] properties = Utils.beanInfo(o.getClass()).getPropertyDescriptors();
+    return Arrays.stream(properties)
+          .filter(property -> !property.getName().equals("class"))
+          .map(property -> '"' + property.getName() + "\": " + toJSON(Utils.invokeMethod(o, property.getReadMethod())))
+          .collect(Collectors.joining(", ", "{", "}"));
   }
 }
