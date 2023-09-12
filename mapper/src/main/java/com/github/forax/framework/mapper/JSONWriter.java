d04d7363938df1c4a052692303aea18d3a85d148
AurelFi <aurelien.fiche.esipe@gmail.com>
2023-09-12 10:29:15 +0200

    Mapper-Q1

--- mapper/src/main/java/com/github/forax/framework/mapper/JSONWriter.java
+++ mapper/src/main/java/com/github/forax/framework/mapper/JSONWriter.java
@@ -2,6 +2,10 @@ package com.github.forax.framework.mapper;
 
 public final class JSONWriter {
   public String toJSON(Object o) {
-    throw new UnsupportedOperationException("TODO");
+    return switch (o) {
+      case null -> "null";
+      case String s -> '"' + s + '"';
+      default -> String.valueOf(o);
+    };
   }
 }
