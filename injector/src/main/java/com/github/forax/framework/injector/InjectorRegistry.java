668d124e5dc6c08aef8d06a163e6131e9ac89b4e
AurelFi <aurelien.fiche.esipe@gmail.com>
2023-09-19 09:50:49 +0200

    Injector Q1

--- injector/src/main/java/com/github/forax/framework/injector/InjectorRegistry.java
+++ injector/src/main/java/com/github/forax/framework/injector/InjectorRegistry.java
@@ -1,5 +1,25 @@
 package com.github.forax.framework.injector;
 
+import java.util.HashMap;
+import java.util.Objects;
+
 public final class InjectorRegistry {
-  // TODO
+
+  /** Storage for registered instances */
+  private final HashMap<Class<?>, Object> registry = new HashMap<>();
+
+  public void registerInstance(Class<?> type, Object instance) {
+    Objects.requireNonNull(type);
+    Objects.requireNonNull(instance);
+    if (null != registry.putIfAbsent(type, instance)) {
+      throw new IllegalStateException("Recipe already registered for " + type.getName());
+    }
+  }
+
+  public Object lookupInstance(Class<?> type) {
+    Objects.requireNonNull(type);
+    var val = registry.get(type);
+    if (val == null) throw new IllegalStateException("Type not present in registry");
+    return val;
+  }
 }
